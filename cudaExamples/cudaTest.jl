1aa92de6c5e9ba786c3662129911b852d8c18f2e
LaurenceYang1218 <laurence.yang1218@gmail.com>
2024-05-27 16:29:36 +0800

    feat: replace quantum circuit simulator

--- cudaExamples/cudaTest.jl
+++ cudaExamples/cudaTest.jl
@@ -8,8 +8,8 @@ using CUDA
 using CUDA: i32
 
 Random.seed!(1)
-n = 6
-pround = 1
+n = 10
+pround = 5
 
 angle = rand(2*pround) .* 2π
 graph = erdos_renyi(n, 0.5)
@@ -18,78 +18,6 @@ mixer = mixer_x(n)
 obj_vals = [maxcut(graph, x) for x in states(n)]
 
 println("n: $(n), pround: $(pround)")
-
-"""
-Apply the Hadamard gate ``H^{\\otimes n} to all qubits in `sv`, modifying `sv` in-place.
-"""
-function applyH!(sv)
-    n = Int(log2(length(sv)))
-    @inbounds for i in 1:n
-        applyH!(sv,i)
-    end
-end
-
-"""
-Apply the Hadamard gate ``H^{\\otimes n} to the `l`th qubit in `sv`, modifying `sv` 
-in-place.
-
-Code modified (with permision) from https://blog.rogerluo.dev/2020/03/31/yany/.
-"""
-function applyH!(sv, l)
-    U11 = 1/sqrt(2); U12 = 1/sqrt(2);
-    U21 = 1/sqrt(2); U22 = -1/sqrt(2);
-    step_1 = 1 << (l - 1)
-    step_2 = 1 << l
-
-    @inbounds if step_1 == 1
-        for j in 0:step_2:size(sv, 1)-step_1
-            ST1 = U11 * sv[j + 1] + U12 * sv[j + 1 + step_1]
-            ST2 = U21 * sv[j + 1] + U22 * sv[j + 1 + step_1]
-
-            sv[j + 1] = ST1
-            sv[j + 1 + step_1] = ST2
-        end
-    elseif step_1 == 2
-        for j in 0:step_2:size(sv, 1)-step_1
-            Base.Cartesian.@nexprs 2 i->begin
-                ST1 = U11 * sv[j + i] + U12 * sv[j + i + step_1]
-                ST2 = U21 * sv[j + i] + U22 * sv[j + i + step_1]
-                sv[j + i] = ST1
-                sv[j + i + step_1] = ST2    
-            end
-        end
-    elseif step_1 == 4
-        for j in 0:step_2:size(sv, 1)-step_1
-            Base.Cartesian.@nexprs 4 i->begin
-                ST1 = U11 * sv[j + i] + U12 * sv[j + i + step_1]
-                ST2 = U21 * sv[j + i] + U22 * sv[j + i + step_1]
-                sv[j + i] = ST1
-                sv[j + i + step_1] = ST2    
-            end
-        end
-    elseif step_1 == 8
-        for j in 0:step_2:size(sv, 1)-step_1
-            Base.Cartesian.@nexprs 8 i->begin
-                ST1 = U11 * sv[j + i] + U12 * sv[j + i + step_1]
-                ST2 = U21 * sv[j + i] + U22 * sv[j + i + step_1]
-                sv[j + i] = ST1
-                sv[j + i + step_1] = ST2    
-            end
-        end
-    else
-        for j in 0:step_2:size(sv, 1)-step_1
-            for i in j:8:j+step_1-1
-                Base.Cartesian.@nexprs 8 k->begin
-                    ST1 = U11 * sv[i + k] + U12 * sv[i + step_1 + k]
-                    ST2 = U21 * sv[i + k] + U22 * sv[i + step_1 + k]
-                    sv[i + k] = ST1
-                    sv[i + step_1 + k] = ST2
-                end
-            end
-        end
-    end
-end
-
 
 function initH(mixer::Mixer{X})
     sv = ones(ComplexF64, mixer.N)/sqrt(mixer.N)
@@ -103,7 +31,7 @@ function RZZGate!(d_sv, d_angles, d_objvals, pidx::Int64)
     rzz_complex = cos(rzz_angle) + sin(rzz_angle)im
 
     in = d_sv[tidx]
-    real_sv = real(in) * real(rzz_complex) - imag(in) * imag(rzz_complex)
+    real_sv = real(in) * real(rzz_complex) + imag(in) * imag(rzz_complex)
     imag_sv = -real(in) * imag(rzz_complex) + imag(in) * real(rzz_complex)
     d_sv[tidx] = real_sv + (imag_sv)im
     
@@ -234,8 +162,6 @@ function circuit!(n::Int64, pround::Int64, sv, angles, mixer, obj_vals, N)
     circuit_kernel(n, pround, d_sv, d_angles, d_mixer, d_objvals, N)
     sv = Array(d_sv)
 
-    # println(sv)
-
     for i in eachindex(sv)
         sv[i] = abs2(sv[i])
         sv[i] *= obj_vals[i]
@@ -251,42 +177,12 @@ function circuit(n::Int64, pround::Int64, angles::Vector{Float64},
     return circuit!(n, pround, sv, angles, mixer.d, obj_vals, mixer.N)
 end
 
-function circuit_gt!(n::Int64, pround::Int64, sv, angles, mixer, obj_vals, N)
-    for i in 1:pround
-        @inbounds for j in eachindex(sv)
-            sv[j] *= exp(-1im*angles[i+pround]*obj_vals[j])
-        end
-        applyH!(sv)
-        @inbounds for j in eachindex(sv)
-            sv[j] *= exp(-1im*angles[i]*mixer[j])
-        end
-        applyH!(sv)
-    end
-
-    # println(sv)
-
-    for i in eachindex(sv)
-        sv[i] = abs2(sv[i])
-        sv[i] *= obj_vals[i]
-    end
-
-    return real(sum(sv))
-end
-
-function circuit_gt(n::Int64, pround::Int64, angles::Vector{Float64}, 
-                            mixer::Mixer{X}, obj_vals::Vector{Int64})
-    sv = initH(mixer)
-    return circuit_gt!(n, pround, sv, angles, mixer.d, obj_vals, mixer.N)
-end
 
 f(x) = -exp_value(x, mixer, obj_vals)/maximum(obj_vals)
 f2(x) = -circuit(n, pround, x, mixer, obj_vals)/maximum(obj_vals)
 
-println("circuit: $(circuit(n, pround, angle, mixer, obj_vals)/maximum(obj_vals))")
-println("circuit_gt: $(circuit_gt(n, pround,angle, mixer, obj_vals)/maximum(obj_vals))")
-println("exp_value: $(exp_value(angle, mixer, obj_vals)/maximum(obj_vals))")
-
-
+println("Approximation ratio circuit: $(circuit(n, pround, angle, mixer, obj_vals)/maximum(obj_vals))")
+println("Approximation ratio exp_value: $(exp_value(angle, mixer, obj_vals)/maximum(obj_vals))")
 
 function g!(G, x)
     G .= 0
@@ -301,35 +197,9 @@ end
 minimizer = optimize(f, g!, angle, BFGS(linesearch=LineSearches.BackTracking()))
 optim_angle = Optim.minimizer(minimizer)
 
-# println("angle: $(angle)")
-# println("optim_angle: $(optim_angle)")
-
 approx = circuit(n, pround, optim_angle, mixer, obj_vals)/maximum(obj_vals)
-println("Approximation ratio circuit: $(approx)")
+println("Approximation ratio circuit (optimized): $(approx)")
 
 approx = exp_value(optim_angle, mixer, obj_vals)/maximum(obj_vals)
-println("Approximation ratio exp_value: $(approx)")
+println("Approximation ratio exp_value (optimized): $(approx)")
 
-
-
-
-
-
-
-
-# function grad!(G::Vector, angles::Vector, mixer::Mixer, obj_vals::AbstractVector, measure::AbstractVector=obj_vals; flip_sign=false)
-#     sv = initH(mixer)
-#     grad!(G, sv, angles, mixer, obj_vals, measure; flip_sign=flip_sign)
-# end
-
-# function grad!(G::Vector, sv::Vector, angles::Vector, mixer::Mixer, obj_vals::AbstractVector, measure::AbstractVector=obj_vals; flip_sign=false)
-#     sv_copy = copy(sv)
-#     sv_gpu = CuArray(sv_copy)
-#     dsv = zeros(ComplexF64, mixer.N)
-#     dsv_gpu = CuArray(dsv)
-#     angles_gpu = CuArray(angles)
-#     G .= 0.0
-#     G_gpu = CuArray(G)
-#     f(a,b) = (flip_sign ? -1 : 1)*circuit_kernel(n, pround, a, b, mixer.d, obj_vals, mixer.N)  # 
-#     @cuda threads=length(sv_copy) Enzyme.autodiff_deferred(Reverse, f, Duplicated(sv_gpu, dsv_gpu), Duplicated(angles_gpu, G_gpu))
-# end
